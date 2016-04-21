d70cd6d16b1853cb2381fa15331701599cc83c46
Semenov <semenov@skeeks.com>
2016-04-21 10:29:23 +0300

    updated

---
+++ console/controllers/MysqlController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 18.03.2016
+ */
+namespace skeeks\cms\dbDumper\console\controllers;
+
+use Yii;
+use yii\console\controllers\MigrateController;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Console;
+use yii\helpers\FileHelper;
+
+/**
+ * Working with the mysql database
+ *
+ * Class DbController
+ * @package skeeks\cms\dbDumper\console\controllers
+ */
+class MysqlController extends \yii\console\Controller
+{
+    public $defaultAction = "dump";
+
+    /**
+     * Restore from the file dump
+     * @param null $fileName The path to the dump file
+     */
+    public function actionRestore($fileName = null)
+    {
+        try
+        {
+            $this->stdout("The installation process is running the database\n");
+            \Yii::$app->dbDumper->restore($fileName);
+            $this->stdout("Dump successfully installed\n", Console::FG_GREEN);
+        } catch(\Exception $e)
+        {
+            $this->stdout("In the process of restoring the dump occurred error: {$e->getMessage()}\n", Console::FG_RED);
+        }
+    }
+
+    /**
+     * Creating a dump
+     */
+    public function actionDump()
+    {
+        try
+        {
+            $result = \Yii::$app->dbDumper->dump();
+            $this->stdout("Dump the database was created successfully: {$result}\n", Console::FG_GREEN);
+        } catch(\Exception $e)
+        {
+            $this->stdout("During the dump error occurred: {$e->getMessage()}\n", Console::FG_RED);
+        }
+    }
+
+}
