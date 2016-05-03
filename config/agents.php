025b5919384925301c08c6275022f3aa1a82b74a
Semenov Alexander <semenov@skeeks.com>
2016-05-04 00:40:37 +0300

    Update agents.php

--- config/agents.php
+++ config/agents.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'dbDumper/db/dump' =>
+    'dbDumper/mysql/dump' =>
     [
         'description'       => \Yii::t('skeeks/dbDumper','Backup Database'),
         'agent_interval'    => 3600*24,
